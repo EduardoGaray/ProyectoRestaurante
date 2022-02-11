32410897e643b2bd6c7550f7150eb273f54d405d
EduardoGaray <garayarnaldos@gmail.com>
2022-02-11 13:01:53 +0100

    1

---
+++ src/main/java/com/restaurante/proyectorestaurante/MainController.java
@@ -0,0 +1,13 @@
+package com.restaurante.proyectorestaurante;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("")
+    public String showHomePage(){
+         return "index";
+    }
+
+}
--- src/main/java/com/restaurante/proyectorestaurante/ProyectoRestauranteApplication.java
+++ src/main/java/com/restaurante/proyectorestaurante/ProyectoRestauranteApplication.java
@@ -9,5 +9,4 @@ public class ProyectoRestauranteApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProyectoRestauranteApplication.class, args);
     }
-
 }
