bb9d1027fe2ec280f81f5a57c99e596f413c5573
EduardoGaray <garayarnaldos@gmail.com>
2022-02-11 12:22:59 +0100

    Initial commit

---
+++ src/main/java/com/restaurante/proyectorestaurante/ProyectoRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.restaurante.proyectorestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRestauranteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoRestauranteApplication.class, args);
+    }
+
+}
